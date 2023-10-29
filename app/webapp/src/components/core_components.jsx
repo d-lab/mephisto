70e454fa7db9db31c707cba883e3e095b2a34983
Cong Thanh Vu <zarelasez@gmail.com>
2023-10-29 18:49:55 +0700

    app: data validation

--- app/webapp/src/components/core_components.jsx
+++ app/webapp/src/components/core_components.jsx
@@ -7,6 +7,60 @@
  */
 
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  registerables
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-dragdata';
+import zoom from 'chartjs-plugin-zoom';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  zoom,
+  ...registerables
+);
+
+const getData = (data, originalData) => {
+  return {
+    type: 'line',
+    labels: Array.from(data.keys()),
+    datasets: [
+      {
+        label: 'Updated',
+        data,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        tension: 0.3,
+        pointHitRadius: 25,
+        dragData: true,
+      },
+      {
+        label: 'Original',
+        data: originalData,
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        tension: 0.3,
+        pointHitRadius: 25,
+        dragData: false,
+      },
+    ]
+  }
+};
+
 
 function OnboardingComponent({onSubmit}) {
   return (
@@ -61,36 +115,136 @@ function Directions({children}) {
 }
 
 function SimpleFrontend({taskData, isOnboarding, onSubmit, onError}) {
-  const [answers, setAnswers] = React.useState({
-    answer1: 'sample1',
-    answer2: '',
+  const [answers, setAnswers] = React.useState([...taskData.data.value]);
+  const [stagingAnswers, setStagingAnswers] = React.useState([...taskData.data.value]);
+
+  const originalData = React.useRef([...taskData.data.value]);
+  React.useEffect(() => {
+
+  }, answers);
+
+  const handleInputChange = (idx, event) => {
+    stagingAnswers[idx] = event.target.value;
+    if (!Number.isNaN(stagingAnswers[idx])) {
+      answers[idx] = +stagingAnswers[idx];
+      setAnswers([...answers]);
+    } else {
+      setStagingAnswers([...stagingAnswers]);
+    }
+  };
+
+  const resetDataChange = (idx) => {
+    answers[idx] = originalData.current[idx];
+    stagingAnswers[idx] = originalData.current[idx];
+    setAnswers([...answers]);
+    setStagingAnswers([...stagingAnswers]);
+  };
+
+  const discardChanges = () => {
+    Array.from(originalData.current.keys()).map((idx) => {
+      answers[idx] = originalData.current[idx];
+      stagingAnswers[idx] = originalData.current[idx];
+    });
+    setAnswers([...answers]);
+    setStagingAnswers([...stagingAnswers]);
+  };
+
+  const modifiedData = React.useMemo(() => {
+    return Array.from(originalData.current.keys()).filter((idx) => {
+      if (originalData.current[idx] !== answers[idx]) {
+        return true;
+      }
+      return false;
+    });
   });
 
-  const handleInputChange = (event) => {
-    const {name, value} = event.target;
-    setAnswers({...answers, [name]: value});
-  };
+  const options = React.useMemo(() => {
+    return {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Data',
+        },
+        zoom: {
+          zoom: {
+            wheel: {
+              enabled: true,
+            },
+            pinch: {
+              enabled: true
+            },
+            mode: 'x',
+          }
+        },
+        dragData: {
+          showTooltip: true,
+          round: 3,
+          onDragStart: function(e, element) {
+          },
+          onDrag: function(e, datasetIndex, index, value) {
+          },
+          onDragEnd: function(e, datasetIndex, index, value) {
+            answers[index] = value;
+            stagingAnswers[index] = value;
+            setStagingAnswers([...stagingAnswers]);
+            setAnswers([...answers]);
+          },
+        }
+      }
+    };
+  });
 
   return (
       <div style={{padding: '50px'}}>
         <div className="p-4">
-          <h2 className="text-2xl font-semibold mb-4">This is sample task</h2>
+          <h2 className="text-2xl font-semibold mb-4">This is sample task 123</h2>
           <div>
-            <label htmlFor="answer2"
-                   className="block text-sm font-medium text-gray-700">Input the number you see here to the box below and submit: <strong>{taskData.data.value}</strong></label>
-            <input
-                type="text"
-                id="answer2"
-                name="answer2"
-                value={answers.answer2}
-                onChange={handleInputChange}
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-            />
+            <Line options={options} data={getData(answers, originalData.current)} />
+            {
+              modifiedData.length > 0 &&
+              <table className="table w-full mt-3">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Original</th>
+                    <th>Updated</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    modifiedData.map(idx => {
+                      return <tr key={idx} className="hover">
+                        <th className="align-middle">{idx + 1}</th>
+                        <td className="align-middle">{originalData.current[idx]}</td>
+                        <td className="align-middle">
+                          <input type="text" className="input input-border input-sm w-full max-w-xs" value={stagingAnswers[idx]} 
+                            onChange={(e) => {handleInputChange(idx, e)}} />
+                        </td>
+                        <td className="align-middle"><button className="btn btn-xs" onClick={() => {
+                          resetDataChange(idx)
+                        }}>Reset</button></td>
+                      </tr>
+                    })
+                  }
+                  
+                </tbody>
+              </table>
+            }
           </div>
-          <button className="btn btn-outline mt-5" onClick={() => {
-            onSubmit(answers);
-          }}>Submit
-          </button>
+          <div className="flex gap-3 mt-5">
+            <button className="btn btn-primary" onClick={() => {
+              onSubmit(answers);
+            }}>Submit</button>
+            <button className="btn btn-outline" onClick={() => {
+              discardChanges();
+            }}>Discard Changes
+            </button>
+          </div>
         </div>
       </div>
   );
